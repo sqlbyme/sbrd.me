693ac601cddcebe14b3d2d127b77f68dd220980b
Mike Edwards <mike@songbirdnest.com>
2013-01-09 12:25:05 -0800

    Corrected problem with json return of shortened url and with base 36 encode of shortened param.

--- snip.rb
+++ snip.rb
@@ -2,18 +2,17 @@
 
 require_relative 'lib/url.rb'
 
-get '/' do redirect "http://songbird.me" end
+get '/' do redirect 'http://songbird.me', 301 end
 
 post '/' do
   uri = URI::parse(params[:original])
   raise "Invalid URL" unless uri.kind_of? URI::HTTP or uri.kind_of? URI::HTTPS
   url = Url.first_or_create(:original => uri.to_s)
   
-  content_type :json
-  { 'url' => url }.to_json
+  { 'url' => url.snipped }.to_json
 end
 
-get '/:url' do redirect Url.first(:id => params[:url] ).original end 
+get '/:url' do redirect Url.first(:id => params[:url].to_i(36) ).original, 301 end 
 
 
 # TODO - add custom 404 page to site to deal with error reporting to users.
