bcb52d2851eddcbac97c492eeb111844a3f8bdd9
Mike Edwards <mike@songbirdnest.com>
2013-01-25 15:59:52 -0800

    Customized routes and behavior for 7digital redirect only.

--- sbrdme.rb
+++ sbrdme.rb
@@ -1,18 +1,5 @@
 %w(sinatra data_mapper json).each  { |lib| require lib}
 
-require_relative 'lib/url.rb'
-
-DataMapper::Logger.new($stdout, :info)
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://root:#{ENV['DB_PWD'] || 'password'}@localhost/sbrdme")
-
-configure :test do
-  DataMapper.setup(:default, "mysql://root:#{ENV['DB_PWD'] || 'password'}@localhost/sbrdme_test")
-  DataMapper.auto_migrate!
-end
-
-#DataMapper.auto_migrate!
-DataMapper.finalize
-
 error do
   content_type :json
   status 500
@@ -26,23 +13,4 @@ not_found do
   'The page you are looking for does not exist.'
 end
 
-get '/*' do redirect 'http://songbird.me', 301 end
-
-post '/' do
-  content_type :json
-
-  host = request.host
-  port = request.port
-  host << ":#{port}" if port != 80
-
-  uri = URI::parse(params[:original])
-  raise "Invalid URL" unless uri.kind_of? URI::HTTP or uri.kind_of? URI::HTTPS
-  url = Url.first_or_create(:original => uri.to_s)
-
-  { 'url' => 'http://' + host + '/' + url.out }.to_json
-end
-
-get '/:url' do
-  url = Url.first(:id => params[:url].to_i(36))
-  url ? redirect(url.original, 301) : raise(Sinatra::NotFound)
-end
+get '/*' do redirect 'http://www.amazon.com/MP3-Music-Download/b/?_encoding=UTF8&node=163856011&tag=songbirdme-20&linkCode=ur2&camp=1789&creative=390957', 301 end
